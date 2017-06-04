aaa800c08581a5badd7dcde81b6bbf5f5281df4d
samux1996 <samuel.henrique1996@gmail.com>
2017-06-04 01:43:45 -0300

    Adição do campo CPF na tela de Currículo

--- application/views/Cadastro_Curriculo.php
+++ application/views/Cadastro_Curriculo.php
@@ -32,6 +32,14 @@
 						</div>
 
 						<div class="form-group">
+							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="nome">CPF <span class="required">*</span>
+							</label>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<input type="text" id="cpf" name="cpf" required="required" class="form-control col-md-7 col-xs-12" data-inputmask="'mask': '999.999.999-99'">
+							</div>
+						</div>
+
+						<div class="form-group">
 							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="datanasc">Data de nascimento <span class="required">*</span>
 							</label>
 							<div class="col-md-6 col-sm-6 col-xs-12">
